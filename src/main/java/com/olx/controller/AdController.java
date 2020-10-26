1ce6563456b14daeb183f644ff933b60e742dad5
mshzidan22 <mshzidan22@yahoo.com>
2020-10-26 13:04:21 +0200

    Signed-off-by: mshzidan22 <mshzidan22@yahoo.com>

--- src/main/java/com/olx/controller/AdController.java
+++ src/main/java/com/olx/controller/AdController.java
@@ -55,7 +55,7 @@ public class AdController {
         return adModelAssembler.toModel(adService.getAd(id));
         }
 
-        @GetMapping("/")
+        @GetMapping("/home")
         public CollectionModel<EntityModel<MiniAdDto>> all () {
 
            return adModelAssembler.toCollectionModel(adService.getHomeAds());
